e443aa2023f74145c42354a325bc4459cedd3639
zantodev <user25@gmx.com>
2022-05-17 21:11:18 -0700

    new js

--- 8KYU/js/find_smallest_int_in_array.js
+++ 8KYU/js/find_smallest_int_in_array.js
@@ -1,14 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
 // FIND THE SMALLEST INTEGER IN ARRAY
 // Given an array of integers your solution should find the smallest integer. For example: [34, 15, 88, 2] should return 2
 class SmallestIntegerFinder {
